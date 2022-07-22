44ae3677747d891a35822389303539e89d80eb53
Sparsh003 <sparsh.dev3@gmail.com>
2022-07-22 19:34:32 +0530

    Data for auto suggestions

---
+++ src/static/static.js
@@ -0,0 +1,22 @@
+export const data = [
+  {
+    id: 1,
+    address: "0xef5323409964067fec899091685b48458f361d84",
+  },
+  {
+    id: 2,
+    address: "0xf69ea6646cf682262e84cd7c67133eac59cef07b",
+  },
+  {
+    id: 3,
+    address: "0x7d7d5680a9e040d43bb43d9cdb41ae758aad83ac",
+  },
+  {
+    id: 4,
+    address: "0x541ceeded157f6590befa1b6a34ab87beda66aa7",
+  },
+  {
+    id: 5,
+    address: "0xf2c3100d5748cb4a330fbc00aee3ebf4458d9eb6",
+  },
+];
